deec434ba3fd7efbde40bf5cb25b094f0ce09101
raquel.cagigas <72089335@Securitas>
2017-06-12 16:29:22 +0200

    ['get']

--- web/index.php
+++ web/index.php
@@ -3,54 +3,56 @@ header('Content-Type: application/json');
 ob_start();
 
 #Parametros Globales
-$vino = "Ninguno";
+$vino = 'Ninguno';
 $nBotellas = 0;
 
 #Parametros Ficticios (BD)
 $stock = array( 'Celeste' => 10, 'Viña Esmeralda' => 10, 'Gran Coronas' => 10, 'Viña Sol' => 10);
-$direccion = "C/Luis Jorge Castaños, 23, 4º Dcha. 28999 Valdecillas de Jarama, Madrid";
+$direccion = 'C/Luis Jorge Castaños, 23, 4º Dcha. 28999 Valdecillas de Jarama, Madrid';
 
 #Obtener Info. Peticion
 $json = file_get_contents('php://input'); 
 $request = json_decode($json, true);
-$action = $request["result"]["action"];
-$parameters = $request["result"]["parameters"];
+$action = $request['result']['action'];
+$parameters = $request['result']['parameters'];
+
+echo $parameters;
 
 switch ($action) 
 {
-    case "nuevo.consultarStock":
+    case 'nuevo.consultarStock':
 		#Parametros
-		$vino = parameters["vino"];
-		$nbotellas = parameters["nbotellas"];
+		$vino = parameters['get']['vino'];
+		$nbotellas = parameters['get']['nbotellas'];
 
-		echo "Petición: " . $nbotellas . " de " . $vino;
-		echo $stock[vino] . " botellas en stock";
+		echo 'Petición: ' . $nbotellas . ' de ' . $vino;
+		echo $stock[vino] . ' botellas en stock';
 		
 		if ($stock[vino] < $nbotellas) 
 		{
-			$outputtext = "Lo sentimos pero solamente nos quedan " . $stock[vino] . " existencias de " . $vino . ", ¿Las quiere?";
-			$contextout = array(array("name"=>"nuevopedido", "lifespan"=>5, "parameters"=>array("vino"=>$vino, "nBotellas"=>$nbotellas, "direccion"=>$direccion,)));
-			$source = "bodegastorres.php";
+			$outputtext = 'Lo sentimos pero solamente nos quedan ' . $stock[vino] . ' existencias de ' . $vino . ', ¿Las quiere?';
+			$contextout = array(array('name'=>'nuevopedido', 'lifespan'=>5, 'parameters'=>array('vino'=>$vino, 'nBotellas'=>$nbotellas, 'direccion'=>$direccion,)));
+			$source = 'bodegastorres.php';
 		} 
 		else
 		{
 			#return 
 			#{
-				#"contextOut": [{"name":"nuevopedido", "lifespan":5, "parameters":{"vino": vino, "nBotellas":nbotellas, "direccion":direccion}}],
-				#"source": "BodegaTorres",
-				#"followupEvent":{"name":"consultarDireccion","data":{"nBotellas":nbotellas, "vino":vino, "direccion": direccion}}
+				#'contextOut': [{'name':'nuevopedido', 'lifespan':5, 'parameters':{'vino': vino, 'nBotellas':nbotellas, 'direccion':direccion}}],
+				#'source': 'BodegaTorres',
+				#'followupEvent':{'name':'consultarDireccion','data':{'nBotellas':nbotellas, 'vino':vino, 'direccion': direccion}}
 			#}
 		}
         break;
-    case "nuevo.confirmarDireccion":
-        echo "Confirmar Direccion";
+    case 'nuevo.confirmarDireccion':
+        echo 'Confirmar Direccion';
         break;
 }
 
-$output["contextOut"] = $contextout;
-$output["speech"] = $outputtext;
-$output["displayText"] = $outputtext;
-$output["source"] = $source;
+$output['contextOut'] = $contextout;
+$output['speech'] = $outputtext;
+$output['displayText'] = $outputtext;
+$output['source'] = $source;
 ob_end_clean();
 echo json_encode($output);
 ?>
