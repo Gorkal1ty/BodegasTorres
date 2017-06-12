e08e9240feabd903acd7b6d7dc965ea3e5348906
raquel.cagigas <72089335@Securitas>
2017-06-12 16:06:32 +0200

    Pruebas PHP

--- web/index.php
+++ web/index.php
@@ -1,21 +1,56 @@
 <?php
 header('Content-Type: application/json');
 ob_start();
+
+#Parametros Globales
+$vino = "Ninguno";
+$nBotellas = 0;
+
+#Parametros Ficticios (BD)
+$province_list = array( 'Celeste' => 10, 'Viña Esmeralda' => 10, 'Gran Coronas' => 10, 'Viña Sol' => 10);
+$direccion = "C/Luis Jorge Castaños, 23, 4º Dcha. 28999 Valdecillas de Jarama, Madrid";
+
+#Obtener Info. Peticion
 $json = file_get_contents('php://input'); 
 $request = json_decode($json, true);
 $action = $request["result"]["action"];
 $parameters = $request["result"]["parameters"];
 
-#[Code to set $outputtext, $nextcontext, $param1, $param2 values]
-$outputtext = "HOLA";
-$param1value = "parametro1";
-$param2value = "parametro2";
+switch ($action) 
+{
+    case "nuevo.consultarStock":
+		#Parametros
+		$vino = parameters.get("vino");
+		$nbotellas = parameters.get("nbotellas");
 
-$output["contextOut"] = array(array("name" => "$next-context", "parameters" =>
-array("param1" => $param1value, "param2" => $param2value)));
+		echo "Petición: " . nbotellas . " de " . vino;
+		echo str(stock[vino]) . " botellas en stock";
+		
+		if (stock[vino] < int(nbotellas)) 
+		{
+			$outputtext = "Lo sentimos pero solamente nos quedan " . str(stock[vino]) . " existencias de " . vino . ", ¿Las quiere?";
+			$contextout = array(array("name"=>"nuevopedido", lifespan=>5, "parameters"=>array("vino"=>$vino, "nBotellas"=>$nbotellas, "direccion"=>$direccion,)));
+			$source = "bodegastorres.php";
+		} 
+		else
+		{
+			#return 
+			#{
+				#"contextOut": [{"name":"nuevopedido", "lifespan":5, "parameters":{"vino": vino, "nBotellas":nbotellas, "direccion":direccion}}],
+				#"source": "BodegaTorres",
+				#"followupEvent":{"name":"consultarDireccion","data":{"nBotellas":nbotellas, "vino":vino, "direccion": direccion}}
+			#}
+		}
+        break;
+    case "nuevo.confirmarDireccion":
+        echo "Confirmar Direccion";
+        break;
+}
+
+$output["contextOut"] = $contextout;
 $output["speech"] = $outputtext;
 $output["displayText"] = $outputtext;
-$output["source"] = "consultarStock.php";
+$output["source"] = $source;
 ob_end_clean();
 echo json_encode($output);
 ?>
