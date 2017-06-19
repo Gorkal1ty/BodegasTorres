50d08323e34d2ede78c4f6556fc967a59a73542c
raquel.cagigas <72089335@Securitas>
2017-06-19 12:41:40 +0200

    test

--- web/js/dataTables.bootstrap.js
+++ web/js/dataTables.bootstrap.js
@@ -35,7 +35,7 @@ $.extend( DataTable.ext.classes, {
 
 
 /* Bootstrap paging button renderer */
-DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, buttons, page, pages ) {
+/*DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, buttons, page, pages ) {
 	var api     = new DataTable.Api( settings );
 	var classes = settings.oClasses;
 	var lang    = settings.oLanguage.oPaginate;
@@ -121,11 +121,11 @@ DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, bu
 		}
 	};
 
-	/*attach(
+	attach(
 		$(host).empty().html('<ul class="pagination"/>').children('ul'),
 		buttons
-	);*/
-};
+	);
+};*/
 
 
 /*
