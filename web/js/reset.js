1aed387b08a83c25065225634b6eebb54eda7af4
raquel.cagigas <72089335@Securitas>
2017-06-20 11:38:29 +0200

    test csv

--- web/js/reset.js
+++
@@ -1,9 +0,0 @@
-function resetStock()
-{
-	var fso = new ActiveXObject("Scripting.FileSystemObject");
-	var fileLoc = '../bd/stock.csv';
-	var file = fso.CreateTextFile(fileLoc, true);
-	file.writeline('NOMBRE,TIPO,PRECIO (€),STOCK');
-	file.Close();
-	alert('File created successfully at location: ' + fileLoc);
-}
