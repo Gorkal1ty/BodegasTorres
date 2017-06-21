715693714b0419001135c5d6d10310a331099229
raquel.cagigas <72089335@Securitas>
2017-06-21 10:59:42 +0200

    INICIO test comando

--- web/js/getCSV.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=\..\bd\"stock.csv");
-$data=stripcslashes($_REQUEST['csv_text']);
-echo $data; 
-?>
